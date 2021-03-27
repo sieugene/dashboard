dc20576a1d4513d37229f08f4460e42add66059e
sieugene <sieugene@mail.ru>
2021-03-28 01:10:58 +0300

    Test fix paths for vercel

--- server/controllers/EditorController.ts
+++ server/controllers/EditorController.ts
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const path = require("path");
+// const path = require("path");
 
+const filePath = "public/data/editor.json";
 class Editor {
   save(editors) {
-    const filePath = path.join("public", "data", "editor.json");
     return new Promise((resolve, reject) => {
       fs.writeFile(filePath, JSON.stringify(editors), (err, data) => {
         if (err) {
@@ -17,15 +17,15 @@ class Editor {
     });
   }
   getAll() {
-    const filePath = path.join("public", "data", "editor.json");
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, function (err, data) {
         if (err) {
           reject(err);
+        } else {
+          setTimeout(() => {
+            resolve(JSON.parse(data));
+          }, 3000);
         }
-        setTimeout(() => {
-          resolve(JSON.parse(data));
-        }, 3000);
       });
     });
   }
